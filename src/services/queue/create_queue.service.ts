ebb465c58644de47f33defafb80f361206db37b4
slear08 <slearmendoza8@gmail.com>
2025-02-06 00:12:47 +0800

    enhancement: added property in medical queue model

--- src/services/queue/create_queue.service.ts
+++ src/services/queue/create_queue.service.ts
@@ -1,6 +1,9 @@
 import MedicalQueue from '../../models/medical_queue.model';
 
-export const createQueueService = async (userId: string) => {
+export const createQueueService = async (
+  userId: string,
+  purpose: 'checkup' | 'medicine-request'
+) => {
   const existingQueue = await MedicalQueue.findOne({ userId });
 
   if (existingQueue) {
@@ -10,6 +13,7 @@ export const createQueueService = async (userId: string) => {
   const queueEntry = new MedicalQueue({
     userId,
     status: 'waiting',
+    purpose,
     timeSchedule: new Date(),
   });
 
