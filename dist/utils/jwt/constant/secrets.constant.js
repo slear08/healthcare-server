a9ac581afc1947639a548804dd85455451d331a6
slear08 <slearmendoza8@gmail.com>
2025-03-31 03:17:33 +0800

    remove dist

--- dist/utils/jwt/constant/secrets.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REFRESH_EXPIRES_IN = exports.REFRESH_SECRET = exports.JWT_EXPIRES_IN = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'secret-key';
-exports.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
-exports.REFRESH_SECRET = process.env.REFRESH_SECRET || 'refresh-secret-key';
-exports.REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN || '30d';
