da5219cf357a7d17915b6383234752e9273d384a
slear08 <slearmendoza8@gmail.com>
2025-03-31 01:45:05 +0800

    fixed missing type

--- src/utils/jwt/sign_refresh_token.util.ts
+++ src/utils/jwt/sign_refresh_token.util.ts
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
-import { JWTPayload } from './interface/jwt_payload.interface';
+
 import {
   REFRESH_EXPIRES_IN,
   REFRESH_SECRET,
 } from './constant/secrets.constant';
+import { JWTPayload } from './interface/jwt_payload.interface';
 
 export const SignRefreshToken = (payload: JWTPayload): string => {
-  return jwt.sign(payload, REFRESH_SECRET, {
+  return jwt.sign(payload, REFRESH_SECRET as jwt.Secret, {
     expiresIn: REFRESH_EXPIRES_IN,
   });
 };
--- src/utils/jwt/sign_token.util.ts
+++ src/utils/jwt/sign_token.util.ts
@@ -1,12 +1,13 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
-import { JWTPayload } from './interface/jwt_payload.interface';
+
 import { JWT_EXPIRES_IN, JWT_SECRET } from './constant/secrets.constant';
+import { JWTPayload } from './interface/jwt_payload.interface';
 
 export const SignJWT = (
   payload: JWTPayload,
   options: SignOptions = {}
 ): string => {
-  return jwt.sign(payload, JWT_SECRET, {
+  return jwt.sign(payload, JWT_SECRET as jwt.Secret, {
     expiresIn: JWT_EXPIRES_IN,
     ...options,
   });
