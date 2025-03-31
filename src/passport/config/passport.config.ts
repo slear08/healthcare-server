8136377affc7d4b61e9928b9c2e079f02b5904de
slear08 <slearmendoza8@gmail.com>
2025-03-31 08:19:29 +0800

    fixed value

--- src/passport/config/passport.config.ts
+++ src/passport/config/passport.config.ts
@@ -13,7 +13,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: '/api/auth/google/callback',
+      callbackURL:
+        'https://healthcare-server-pa9l.onrender.com/api/auth/google/callback',
       scope: ['profile', 'email'],
       passReqToCallback: true,
     },
