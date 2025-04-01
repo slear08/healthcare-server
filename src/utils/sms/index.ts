c370f38c14e2da9072fc48624501944b10e50682
slear08 <slearmendoza8@gmail.com>
2025-04-01 08:12:52 +0800

    added sms integration

--- src/utils/sms/index.ts
+++ src/utils/sms/index.ts
@@ -1,28 +1,31 @@
-import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 import User from '../../models/user.model';
+import log from '../logger';
 
 export async function SendSMSUtil(userId: string, message: string) {
   const user = await User.findById(userId);
 
   try {
-    const response = await axios.post(
-      process.env.SEMAPHORE_HOST!,
-      new URLSearchParams({
-        apikey: process.env.SEMAPHORE_API_KEY!,
-        number: user?.mobileNumber || '',
-        message: message,
+    const response = await fetch('https://api.httpsms.com/v1/messages/send', {
+      method: 'POST',
+      headers: {
+        'x-api-Key': process.env.SMS_API_KEY!,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: `Hello ${user?.name}, ito ay paalala na kailangan mo na pong inumin mo ang gamot mo na ${message}. Maraming salamat!`,
+        encrypted: false,
+        from: process.env.SMS_SENDER_NUMBER!,
+        request_id: uuidv4(),
+        send_at: new Date().toISOString(),
+        to: `${user?.mobileNumber}`,
       }),
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
+    });
 
-    return response.data;
+    await response.json();
   } catch (error) {
-    console.error('Error sending SMS:', error);
+    log.error('Error sending SMS:', error);
     throw error;
   }
 }
